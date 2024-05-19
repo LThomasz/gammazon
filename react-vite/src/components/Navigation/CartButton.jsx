0f5c97f92cdad7d56bafe356a29a4d630c9d0483
Lyndon <tzlyndon@gmail.com>
2024-05-18 20:38:13 -0700

    re added cart button functionality

--- react-vite/src/components/Navigation/CartButton.jsx
+++ react-vite/src/components/Navigation/CartButton.jsx
@@ -1,22 +1,14 @@
-// import { useNavigate } from "react-router-dom";
-// import shopping from "../../../images/shopping.png"
+import { useNavigate } from "react-router-dom";
+import shopping from "../../../images/shopping.png"
 import "./Navigation.css"
-import OpenModalButton from "../OpenModalButton/OpenModalButton";
+
 function  CartButton() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <>
       <div className="cart-link-container">
-        {/* <img src={shopping} alt="" className="shopping-cart" onClick={() => navigate('/cart')}/> */}
-        <OpenModalButton
-          buttonText={"Cart"}
-          modalComponent={
-            <div style={{height: '40px', width: '180px', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', fontWeight: 'bold'}}>
-              <p>Feature Coming Soon</p>
-            </div>
-          }
-        ></OpenModalButton>
+        <img src={shopping} alt="" className="shopping-cart" onClick={() => navigate('/cart')}/>
       </div>
     </>
   )
